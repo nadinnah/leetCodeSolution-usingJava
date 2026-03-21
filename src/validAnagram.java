1cd2687f63389df81937af27031be9e0d72d4f1f
nadinnah <nadinahmedmohamed@gmail.com>
2026-03-21 21:41:05 +0300

    valid anagram solution

---
+++ src/validAnagram.java
@@ -0,0 +1,19 @@
+public class validAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] sToCharArr= s.toCharArray(); // a n a g r a m
+        char[] tToCharArr= t.toCharArray();// n a g a r a m
+        int[] isAnagram= new int[26];
+
+        for(int i=0; i<sToCharArr.length;i++){
+            isAnagram[sToCharArr[i]-'a']+=1;
+            isAnagram[tToCharArr[i]-'a']-=1;
+        }
+
+        for (int count : isAnagram) {
+            if (count != 0) return false;
+        }
+
+        return true;
+
+    }
+}
