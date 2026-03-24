9f4b160f16b0e1dfd2f643249eb7645d59a07d3e
nadinnah <nadinahmedmohamed@gmail.com>
2026-03-24 21:10:41 +0300

    merge interval solution

---
+++ src/mergeInterval.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class mergeInterval {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[0][0];
+        List<int[]> result= new ArrayList<>();
+        Arrays.sort(intervals, (a, b)->a[0]-b[0]);
+        int[] currentInterval=intervals[0];
+        for(int i=1; i<intervals.length;i++){
+            if(intervals[i][0]<=currentInterval[1]){ //overlap
+                currentInterval[1]= Math.max(intervals[i][1],currentInterval[1]);
+            }else {
+                result.add(currentInterval);
+                currentInterval=intervals[i];
+            }
+        }
+        result.add(currentInterval);
+
+        return result.toArray(new int[result.size()][] );
+
+    }
+}
